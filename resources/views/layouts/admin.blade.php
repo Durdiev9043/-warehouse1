2b5c19d1fe33144978659a24ee5088d146da18c7
durdiev9043 <donyorbek9043@gmail.com>
2022-05-17 11:44:14 +0500

    pages

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -73,7 +73,7 @@
 {{--            </div>--}}
 {{--        </nav>--}}
 {{--    </div>--}}
-    <div class="navbar">
+    <div class="navbar ">
         <div class="container-fluid">
             <div class="navbar-header">
                 <div class="d-flex">
@@ -143,16 +143,23 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings icon-lg"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
                     </div>
                     <div class="drop-btn1">
-                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button type="button"  class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://themesdesign.in/dason-node/layouts/default/assets/images/users/avatar-1.jpg" alt="" width="31" height="31" class="radius">
                             <span>Paul K. </span>
                             <span class="iconify" data-icon="ep:arrow-down" style="color: white;" data-width="12" data-height="19.5"></span>
                         </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
+                        <ul class="dropdown-menu dropdown-menu-end" >
                             <li><button class="dropdown-item" type="button">Action</button></li>
                             <li><button class="dropdown-item" type="button">Another action</button></li>
                             <li><button class="dropdown-item" type="button">Something else here</button></li>
                         </ul>
+                    </div>
+                    <div class="drop-btn">
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button>chiqish</button>
+                        </form>
+
                     </div>
 
                 </div>
